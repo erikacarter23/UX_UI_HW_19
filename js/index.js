91dc5a01bd90c8787a69d6740d52b160f4218260
erikacarter23 <erikarcarter23@gmail.com>
2024-04-14 18:04:41 -0500

    Java Fix

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-  $(".card1-dropdown").click(function(){
-    $(".card1").slideDown("slow");
+$(document).ready(function(){
+    function myFunction() {
+      var x = document.getElementById("myTopnav");
+      if (x.className === "topnav") {
+        x.className += " responsive";
+      } else {
+        x.className = "topnav";
+      }
+    }
+    $(".card1-dropdown").click(function(){
+    $(".card1b").slideDown("slow");
   });
   $(".card-dropdown").click(function(){
-    $(".card1").slideUp("slow");
+    $(".card1b").slideUp("slow");
   });
+});
